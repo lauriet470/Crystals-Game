15310923444736a75b75c207f6a368eff9e419c5
lauriet470 <56237958+lauriet470@users.noreply.github.com>
2020-05-21 18:33:04 -0500

    repaired javascript

--- javascript/game.js
+++ javascript/game.js
@@ -1,10 +1,13 @@
-<script>
+$( document ).ready(function() {
+ 
 
 
 var score = 0;
-var targetNumber = 100
-var numberOptions = [10, 5, 3, 7];
-
+var targetNumber = Math.floor(Math.random()*100)+1; 
+var bluegem=10;
+var yellowgem=7;
+var redgem=5;
+var greengem=3;
 $(".scoreboard").text(score);
 $(".targetscore").text(targetNumber);
 
@@ -16,30 +19,30 @@ $(".btn").on("click", function(){
        
 var myId=$(this).attr("id");
    if(myId==="bluegem"){
-score += numberOptions[10];
+score += bluegem;
 console.log("bluegem Clicked");
 
 }
 else if (myId==="yellowButton"){
-score += numberOptions[3];
-console.log("yellowButton Clicked");
-
+score += yellowgem;
+console.log("yellowButton clicked");
 }
-else if(myId==="greenie"){
-score += numberOptions[5];
-console.log("greenie clicked");
-}
-else if(myId=="ruby"){
-score += numberOptions[7];
+else if(myId==="ruby"){
+score += redgem;
 console.log("ruby clicked");
+}
+else if (myId==="greenie"){
+   score+= greengem
+   console.log("greenie clicked");
 }
 if(score === targetNumber){
 alert("You Win!");
 
 score=0;
-targetNumber=mathRandom(max);
-}
+targetNumber=Math.floor(Math.random()*100)+1;
 $(".scoreboard").text(score);
-$(".targetscore").text(targetNumber);
+$(".targetscore").text(targetNumber);}
 });
-</script>
+console.log( "ready!" );
+});
+
